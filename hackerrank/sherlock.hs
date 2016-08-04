6d95d9df10b2a1d6d0047577604a540cee91c3d2
paolino <paolo.veronelli@gmail.com>
2016-08-04 13:48:23 +0200

    .

--- hackerrank/sherlock.hs
+++ hackerrank/sherlock.hs
@@ -1,8 +1,15 @@
-t :: [[([Int],[Int])]]
-t = repeat (1:repeat 0,repeat 0) : map g t
+import Control.Monad.Fix 
+import Control.Monad
+import Control.Applicative
 
-g :: [([Int],[Int])] ->  [([Int],[Int])]
-g t = let
-    li = (repeat 0 ,1:repeat 0) : zipWith f li t 
-    f (l,u) (l',u') = (zipWith (+) l (0:u), zipWith (+) (0:l') u')
-    in li
+table :: [[([Int],[Int])]]
+table = fix $ (:) (repeat (1:repeat 0,repeat 0)) . map g where
+    g t = fix $ (:) (repeat 0 ,1:repeat 0) . zipWith f (tail t) 
+    f (l',u') (l,u) = (zipWith (+) l (0:u), zipWith (+) (0:l') u')
+
+main = do
+    n <- readLn
+    xs <- replicateM n $ map read <$> words <$> getLine
+    forM_ xs $ \[r,c,l] -> print $ 
+        sum . take (l + 1) . uncurry (zipWith (+)) 
+            $ table !! (r - 1) !! (c - 1)
